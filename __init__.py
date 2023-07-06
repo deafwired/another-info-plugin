d97c343b27d92c30cbbda75be0baff788c7b967f
DeafWired <37221707+DeathWired@users.noreply.github.com>
2023-07-05 22:48:14 -0400

    Fixing the /stats command

--- __init__.py
+++ __init__.py
@@ -16,22 +16,22 @@ JSONFORMAT = "Backup_%b_%d_%Y_%H_%M"
 class Plugin(BasePlugin):
     def __init__(self):
         super().__init__()
-        self.settings = {
-            "saveInterval" : 24
-        }
+        self.settings = {"saveInterval": 24}
         self.metasettings = {
-            "saveInterval" : {
-                "description" : "Choose how often plugin saves data (hr)",
-                "type" : "integer"
+            "saveInterval": {
+                "description": "Choose how often plugin saves data (hr)",
+                "type": "integer",
             }
         }
-        self.__publiccommands__ = self.__privatecommands__ = [
-            ("stats", self.showStats)]
+        self.__publiccommands__ = self.__privatecommands__ = [("stats", self.showStats)]
         BASE_PATH = Path(__file__).parent
         self.BASE_PATH = BASE_PATH
         self.jsonPath = os.path.join(BASE_PATH, "json")
-        self.template = open(os.path.join(BASE_PATH,"template.html"),"r")
-        load = open(os.path.join(self.jsonPath, "currentJson.json"), "r", encoding="utf-8")
+        with open(os.path.join(BASE_PATH, "template.html"), "r") as file:
+            self.template = file.readlines()
+        load = open(
+            os.path.join(self.jsonPath, "currentJson.json"), "r", encoding="utf-8"
+        )
         self.stats = json.load(load)
         load.close()
         self.scheduling()
@@ -47,38 +47,38 @@ class Plugin(BasePlugin):
     def upload_finished_notification(self, user, virtual_path, real_path):
         try:
             self.stats["files"][real_path]["count"] += 1
-            self.stats["files"][real_path]["total_bytes"] += self.stats["files"][real_path]["bytes"]
+            self.stats["files"][real_path]["total_bytes"] += self.stats["files"][
+                real_path
+            ]["bytes"]
         except:
             self.stats["files"][real_path] = {}
             self.stats["files"][real_path]["count"] = 1
-            self.stats["files"][real_path]["bytes"] = os.path.getsize(
-                real_path)
-            self.stats["files"][real_path]["total_bytes"] = self.stats["files"][real_path]["bytes"]
+            self.stats["files"][real_path]["bytes"] = os.path.getsize(real_path)
+            self.stats["files"][real_path]["total_bytes"] = self.stats["files"][
+                real_path
+            ]["bytes"]
         self.stats["files"][real_path]["last_user"] = user
         try:
             self.stats["users"][user]["total"] += 1
             self.stats["users"][user]["last_file"] = real_path
-            self.stats["users"][user]["total_bytes"] += os.path.getsize(
-                real_path)
+            self.stats["users"][user]["total_bytes"] += os.path.getsize(real_path)
         except:
             self.stats["users"][user] = {}
             self.stats["users"][user]["total"] = 1
             self.stats["users"][user]["last_file"] = real_path
-            self.stats["users"][user]["total_bytes"] = os.path.getsize(
-                real_path)
+            self.stats["users"][user]["total_bytes"] = os.path.getsize(real_path)
         self.stats["day"][self.getDayofWeek()] += 1
 
     def showStats(self, *args):
-        makeHTML(self.stats,self.template, os.path.join(self.BASE_PATH, "html"))
-        webbrowser.open(
-            "file://"+os.path.join(self.BASE_PATH, "html","index.html"))
+        makeHTML(self.stats, self.template, os.path.join(self.BASE_PATH, "html"))
+        webbrowser.open("file://" + os.path.join(self.BASE_PATH, "html", "index.html"))
 
     def saveJson(self, json_path, stats):
-        path = json_path+"\\"+self.formatDate()+".json"
+        path = json_path + "\\" + self.formatDate() + ".json"
         f = open(path, "w")
         json.dump(stats, f)
         f.close()
-        f = open(json_path+"\\currentJson.json", "w")
+        f = open(json_path + "\\currentJson.json", "w")
         json.dump(stats, f)
         f.close()
 
@@ -93,7 +93,8 @@ class Plugin(BasePlugin):
     def scheduling(self):
         self.saveJson(self.jsonPath, self.stats)
         self.timer = threading.Timer(
-            3600*self.settings["saveInterval"], self.scheduling)
+            3600 * self.settings["saveInterval"], self.scheduling
+        )
         self.timer.start()
 
     def log(self, *msg):
--- makehtml.py
+++ makehtml.py
@@ -1,44 +1,83 @@
-import json
-import re
 import os
+import re
 from itertools import islice
 
 
 class makeHTML:
     def __init__(self, data, template, PATH):
         self.data = data
-        self.template = template
         topFiveUsers, topFiveFiles = self.topFive(self.data)
-        x = template.readlines()
-        files = list(data["files"].keys())
-        for i in range(len(topFiveFiles)):
+        # have to make a coy because otherwise you cant run the command more than once
+        self.htmlLines = template[:]
+        for i in range(len(self.htmlLines)):
+            if "{TOPFILES}" in self.htmlLines[i]:
+                self.topFiles(i, topFiveFiles)
+                w = i
+                break
+        for i in range(w, len(self.htmlLines)):
+            if "{TOPUSERS}" in self.htmlLines[i]:
+                self.topUsers(i, topFiveUsers)
+                w = i
+                break
+        for i in range(w, len(self.htmlLines)):
+            if "{FILETABLE}" in self.htmlLines[i]:
+                self.addSongs(i, self.data["files"])
+                w = i
+                break
+        for i in range(w, len(self.htmlLines)):
+            if "{USERTABLE}" in self.htmlLines[i]:
+                self.addUser(i, self.data["users"])
+        # deleting the template tags
+        tags = ["{TOPFILES}", "{TOPUSERS}", "{FILETABLE}", "{USERTABLE}"]
+        for tag in tags:
+            for i in range(len(self.htmlLines)):
+                if tag in self.htmlLines[i]:
+                    self.htmlLines.pop(i)
+                    break
+        # writing the html file
+        with open(os.path.join(PATH, "test.html"), "w") as f:
+            f.writelines(self.htmlLines)
+            f.close()
+
+    # add the top five files
+    def topFiles(self, pos, data):
+        keys = [self.stripPath(key) for key in data.keys()]
+        items = list(data.values())
+        for i in range(len(data)):
             text = (
                 "          <li>"
-                + self.stripPath(list(topFiveFiles.keys())[i])
-                + "<span class='topFive'>" +
-                str(list(topFiveFiles.values())[i])+"</span></li>\n"
-            )
-            x.insert(32 + i, text)
-        x.pop(32 + len(topFiveFiles))
-        for i in range(len(topFiveUsers)):
-            text = (
-                "          <li>"
-                + list(topFiveUsers.keys())[i]
+                + str(keys[i])
                 + "<span class='topFive'>"
-                + str(list(topFiveUsers.values())[i])
+                + str(items[i])
                 + "</span></li>\n"
             )
-            x.insert(38 + len(topFiveFiles) + i, text)
-        x.pop(37 + len(topFiveFiles))
-        for i in range(len(data["files"])):
-            downloads = data["files"][files[i]]["count"]
-            totalBytes = data["files"][files[i]]["total_bytes"]
-            fileBytes = data["files"][files[i]]["bytes"]
-            lastUser = data["files"][files[i]]["last_user"]
+            self.htmlLines.insert(pos + i, text)
+
+    # add the top five users
+    def topUsers(self, pos, data):
+        keys = [key for key in data.keys()]
+        items = list(data.values())
+        for i in range(len(data)):
+            text = (
+                "          <li>"
+                + str(keys[i])
+                + "<span class='topFive'>"
+                + str(items[i])
+                + "</span></li>\n"
+            )
+            self.htmlLines.insert(pos + i, text)
+
+    # add the songs to the table
+    def addSongs(self, pos, data):
+        for i in data:
+            downloads = data[i]["count"]
+            totalBytes = data[i]["total_bytes"]
+            fileBytes = data[i]["bytes"]
+            lastUser = data[i]["last_user"]
             href = "#" + lastUser
             text = (
                 "        <tr><td>"
-                + self.stripPath(files[i])
+                + self.stripPath(i)
                 + "</td><td>"
                 + str(downloads)
                 + '</td><td bytes="'
@@ -56,19 +95,20 @@ class makeHTML:
                 + lastUser
                 + "</a></td></tr>\n"
             )
-            x.insert(51 + len(topFiveFiles)+ len(topFiveUsers) + i, text)
-        x.pop(51 + len(topFiveFiles) + len(topFiveUsers) + len(data["files"]))
-        users = list(data["users"].keys())
-        for i in range(len(data["users"])):
-            downloads = data["users"][users[i]]["total"]
-            totalBytes = data["users"][users[i]]["total_bytes"]
-            last_file = data["users"][users[i]]["last_file"]
+            self.htmlLines.insert(pos, text)
+
+    # add the users to the table
+    def addUser(self, pos, data):
+        for i in data:
+            downloads = data[i]["total"]
+            totalBytes = data[i]["total_bytes"]
+            last_file = data[i]["last_file"]
             # i love writing html in python, it just looks so clean and simple
             text = (
                 "        <tr><td id='"
-                + users[i]
+                + i
                 + "'>"
-                + users[i]
+                + i
                 + "</td><td>"
                 + str(downloads)
                 + '</td><td bytes="'
@@ -79,13 +119,9 @@ class makeHTML:
                 + self.stripPath(last_file)
                 + "</td></tr>\n"
             )
-            x.insert(65 + len(topFiveFiles) + len(topFiveUsers) + len(data["files"]) + i, text)
-        x.pop(65 + len(topFiveFiles) + len(topFiveUsers) + len(data["files"]) + len(data["users"]))
+            self.htmlLines.insert(pos, text)
 
-        with open(os.path.join(PATH, "index.html"), "w") as f:
-            f.writelines(x)
-            f.close()
-
+    # checks if the path is windows or unix and strips the path based on if its shorter
     def stripPath(self, path):
         windowsStrip = re.compile(r".+\\")
         windowPath = windowsStrip.sub("", path)
@@ -99,14 +135,14 @@ class makeHTML:
         users = {}
         for i in data["users"]:
             users[i] = data["users"][i]["total"]
-        topFiveUsers = dict(
-            sorted(users.items(), key=lambda x: x[1], reverse=True))
+        topFiveUsers = dict(sorted(users.items(), key=lambda x: x[1], reverse=True))
         files = {}
         for i in data["files"]:
             files[i] = data["files"][i]["count"]
-        topFiveFiles = dict(
-            sorted(files.items(), key=lambda x: x[1], reverse=True))
-        return dict(islice(topFiveUsers.items(), 5)), dict(islice(topFiveFiles.items(), 5))
+        topFiveFiles = dict(sorted(files.items(), key=lambda x: x[1], reverse=True))
+        return dict(islice(topFiveUsers.items(), 5)), dict(
+            islice(topFiveFiles.items(), 5)
+        )
 
     def bytesToStr(self, num):
         units = ["Bytes", "KB", "MB", "GB", "TB"]
@@ -116,6 +152,3 @@ class makeHTML:
             unit_index += 1
         formatted_value = "{:.2f}".format(num)
         return f"{formatted_value} {units[unit_index]}"
-
-
-
